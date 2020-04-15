1111894540ae0dae0145a86f687b849daa250cd2
Mitchell Zeiter <zeiter.14@osu.edu>
2020-04-15 10:45:52 -0400

    added extra credit - customizing page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 })
 
 /**
- * Respond with hello worker text
+ * Respond with randomly chosen URL from API, with modified HTML
  * @param {Request} request
  */
 async function handleRequest(request) {
@@ -11,14 +11,45 @@ async function handleRequest(request) {
   // fetch the json variants from the api
   response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
   json_response = await response.json();
-  vars = json_response['variants'];
+  vars_arr = json_response['variants'];
 
   // randomly choose one of the two URL's
   rand_url = Math.random() < 0.5 ? 0 : 1;
-  response = await fetch(vars[rand_url]);
-  
+  response = await fetch(vars_arr[rand_url]);
 
-  return new Response('Hello worker!', {
+
+  // using HTMLRewriter to modify content of some HTML elements */
+  const modified_html = new HTMLRewriter()
+  	.on('title', new ElementHandler("Cloudflare Full-stack Internship Project"))
+  	.on('h1#title', new ElementHandler("Mitchell Zeiter's Cloudflare Project"))
+  	.on('p#description', new ElementHandler("Hello! This is my Cloudflare full-stack internship project."))
+  	.on('a#url', new ElementHandler("Connect with me on LinkedIn!"))
+    .on('a#url', new ElementHandler('https://www.linkedin.com/in/mitchell-zeiter/', 'href'))
+  	.transform(response)
+
+  
+  	const modified_html_text = await modified_html.text()
+
+  return new Response(modified_html_text, {
     headers: { 'Content-type': 'text/html' },
   })
 }
+
+
+/* ElementHandler class used to change text and attributes of HTML  */
+class ElementHandler {
+
+  constructor(newContent, attribute) {
+    this.newContent = newContent
+    this.attribute = attribute
+  }
+
+  element(element) {
+    if (!this.attribute) {
+      element.setInnerContent(this.newContent);
+    } else { 
+      element.setAttribute(this.attribute, this.newContent);
+    }
+  }
+}
+
